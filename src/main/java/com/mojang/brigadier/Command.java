02f3a68ac07aa771bd4df8f542043c85ada6325a
Nathan Adams <dinnerbone@dinnerbone.com>
2017-08-15 09:35:09 +0200

    Commands can throw exceptions

--- src/main/java/com/mojang/brigadier/Command.java
+++ src/main/java/com/mojang/brigadier/Command.java
@@ -1,10 +1,11 @@
 package com.mojang.brigadier;
 
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandException;
 
 @FunctionalInterface
 public interface Command<S> {
     int SINGLE_SUCCESS = 1;
 
-    int run(CommandContext<S> context);
+    int run(CommandContext<S> context) throws CommandException;
 }
