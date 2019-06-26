4b2ee6b86d0adaa0007ee22f9426ee3d518b1c3a
xDark <aleshkailyashevich@gmail.com>
2019-06-26 17:59:41 +0300

    Allow Russian letters in commands

--- src/main/java/com/mojang/brigadier/StringReader.java
+++ src/main/java/com/mojang/brigadier/StringReader.java
@@ -170,6 +170,8 @@ public class StringReader implements ImmutableStringReader {
         return c >= '0' && c <= '9'
             || c >= 'A' && c <= 'Z'
             || c >= 'a' && c <= 'z'
+            || c >= 'а' && c <= 'я'
+            || c >= 'А' && c <= 'Я'
             || c == '_' || c == '-'
             || c == '.' || c == '+';
     }
