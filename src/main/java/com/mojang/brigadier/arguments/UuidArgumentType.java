b0d470b51d0b64b084f5894bb17a86caa0e918ec
xDark <aleshkailyashevich@gmail.com>
2019-07-05 11:41:12 +0300

    Uuid type argument support

---
+++ src/main/java/com/mojang/brigadier/arguments/UuidArgumentType.java
@@ -0,0 +1,19 @@
+package com.mojang.brigadier.arguments;
+
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+
+import java.util.UUID;
+
+public class UuidArgumentType implements ArgumentType<UUID> {
+
+    public static UuidArgumentType uuid() {
+        return new UuidArgumentType();
+    }
+
+    @Override
+    public UUID parse(StringReader reader) throws CommandSyntaxException {
+        return UUID.fromString(reader.readUnquotedString()
+                .replaceFirst("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5"));
+    }
+}
