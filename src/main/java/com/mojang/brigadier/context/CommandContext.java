53abaed7def963f020ae9efe61aefde369da7f6d
xDark <aleshkailyashevich@gmail.com>
2019-07-05 12:13:05 +0300

    Use Objects#equals where possible

--- src/main/java/com/mojang/brigadier/context/CommandContext.java
+++ src/main/java/com/mojang/brigadier/context/CommandContext.java
@@ -10,6 +10,7 @@ import com.mojang.brigadier.tree.CommandNode;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class CommandContext<S> {
 
@@ -103,11 +104,9 @@ public class CommandContext<S> {
         if (!arguments.equals(that.arguments)) return false;
         if (!rootNode.equals(that.rootNode)) return false;
         if (nodes.size() != that.nodes.size() || !nodes.equals(that.nodes)) return false;
-        if (command != null ? !command.equals(that.command) : that.command != null) return false;
+        if (!Objects.equals(command, that.command)) return false;
         if (!source.equals(that.source)) return false;
-        if (child != null ? !child.equals(that.child) : that.child != null) return false;
-
-        return true;
+        return Objects.equals(child, that.child);
     }
 
     @Override
